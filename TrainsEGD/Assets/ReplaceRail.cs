bd0cb9f4e7942aec428a4035880c95feda9a239f
Spencer <johnss18@rpi.edu>
2016-02-15 23:32:19 -0500

    Train "Moves" and Jostles now

---
+++ TrainsEGD/Assets/ReplaceRail.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReplaceRail : MonoBehaviour
+{
+	void Update ()
+	{
+		if(transform.localPosition.x <= -90)
+		{
+			transform.localPosition = new Vector3(30,transform.localPosition.y,transform.localPosition.z);
+		}
+	}
+}
